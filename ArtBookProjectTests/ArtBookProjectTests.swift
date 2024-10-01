d4ce08cfef588a93a53a7edc2c7ad9d85612af86
habildem <habildmrc@gmail.com>
2024-10-02 00:56:03 +0300

    Initial Commit

---
+++ ArtBookProjectTests/ArtBookProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBookProjectTests.swift
+//  ArtBookProjectTests
+//
+//  Created by Habil Demirci on 2.10.2024.
+//
+
+import Testing
+@testable import ArtBookProject
+
+struct ArtBookProjectTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
